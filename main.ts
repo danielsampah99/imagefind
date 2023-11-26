3688d9488efeb30e83c217fa02c455ef29dda668
danielsampah99 <danielsampah99@gmail.com>
2023-11-26 02:13:14 +0000

    Update: Application now handles errors better.

--- main.ts
+++ main.ts
@@ -1,69 +1,84 @@
 // Access key from unsplash.com to access their api.
-const accessKey = "R8VF5DtWaupfudeO9XsvbtXuc-ENVeDOt7jxPqY3OBI";
+import * as console from "console";
+
+const accessKey: string = "R8VF5DtWaupfudeO9XsvbtXuc-ENVeDOt7jxPqY3OBI";
 
 // Representing html elements in javascript.
 const formElement = document.querySelector("form") as HTMLFormElement;
 const inputElement = document.getElementById("searchInput") as HTMLInputElement;
-const searchResults = document.querySelector(".search--results");
-const showMore = document.getElementById("show-more--button");
+const searchResults = document.querySelector(".search--results") as HTMLElement;
+const showMore = document.getElementById(
+	"show-more--button",
+) as HTMLButtonElement;
 
-let inputData = "";
+let inputData: string = "";
 let page: number = 1;
 
 // Function to control image search.
 async function searchImages() {
 	inputData = inputElement.value;
-	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+	const url: any = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-	// Fetch data from the api.
-	const response = await fetch(url);
+	try {
+		// Fetch data from the api.
+		const response = await fetch(url);
 
-	if (!response.ok) {
-		throw new Error(`Failed to fetch data: ${response.status}`);
-	}
-
-	// Parse the JSON response.
-	const data = await response.json();
-
-	const results = data.result;
-
-	console.log(results);
-
-	if (page === 1) {
-		searchResults.innerHTML = "";
-	}
-
-	// console.log(results);
-
-	results.forEach((result) => {
-		const imageWrapper = document.createElement("div");
-		imageWrapper.classList.add("search-result");
-
-		const image = document.createElement("img");
-		image.setAttribute("src", result.urls.full);
-
-		if (result.alt_description) {
-			image.alt = result.alt_description;
-		} else {
-			image.alt = "Image Description Is Not Available At This Time.";
+		if (!response.ok) {
+			throw new Error(`Failed to fetch data: ${response.status}`);
 		}
 
-		const imageLink = document.createElement("a");
-		imageLink.href = result.links.html;
-		imageLink.target = "_blank";
-		imageLink.textContent = result.alt_description;
+		// Parse the JSON response.
+		const data = await response.json();
 
-		imageWrapper.appendChild(image);
-		imageWrapper.appendChild(imageLink);
-		searchResults.appendChild(imageWrapper);
-	});
+		//  Check if results is defined and is an array.
+		const results = (data.results || data) as any[];
+		if (Array.isArray(results)) {
+			return console.error(
+				`Invalid or missing results in the API's response.`,
+			);
+		}
 
-	// Increment the page number
-	page++;
+		console.log(results);
 
-	// Show the "Show More" button if on page 2 or higher
-	if (page > 1) {
-		showMore.style.display = "block";
+		if (page === 1) {
+			searchResults.innerHTML = "";
+		}
+
+		// console.log(results);
+
+		results.forEach((result) => {
+			const imageWrapper = document.createElement("div");
+			imageWrapper.classList.add("search-result");
+
+			const image = document.createElement("img");
+			image.setAttribute("src", result.urls.full);
+
+			if (result.alt_description) {
+				image.alt = result.alt_description;
+			} else {
+				image.alt = "Image Description Is Not Available At This Time.";
+			}
+
+			const imageLink = document.createElement("a");
+			imageLink.href = result.links.html;
+			imageLink.target = "_blank";
+			imageLink.textContent = result.alt_description || `No description`;
+
+			imageWrapper.appendChild(image);
+			imageWrapper.appendChild(imageLink);
+			searchResults.appendChild(imageWrapper);
+		});
+
+		// Increment the page number
+		page++;
+
+		// Show the "Show More" button if on page 2 or higher
+		if (page > 1) {
+			showMore.style.display = "block";
+		}
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+		throw error;
 	}
 }
 
